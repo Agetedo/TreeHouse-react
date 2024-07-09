4a47f560289d5f439d326d8d587196b59a290195
Agetedo <agetedo@gmail.com>
2024-07-09 15:09:44 +0300

    first commit

---
+++ src/DataBase/socialLinks.js
@@ -0,0 +1,13 @@
+export const socialLinks = [{
+    id: "socL01",
+    linlText: "SocialLink",
+    linkTo: "https://www.google.com/",
+}, {
+    id: "socL02",
+    linlText: "SocialLink",
+    linkTo: "https://www.google.com/",
+}, {
+    id: "socL03",
+    linlText: "Vip group",
+    linkTo: "https://web.telegram.org/",
+}];
---
+++ src/DataBase/socialMemu.js
@@ -0,0 +1,13 @@
+export const socialMenu = [{
+    id: "sm01",
+    linlText: "SocialLink",
+    linkTo: "https://www.google.com/",
+}, {
+    id: "sm02",
+    linlText: "SocialLink",
+    linkTo: "https://www.google.com/",
+}, {
+    id: "sm03",
+    linlText: "Pinterest",
+    linkTo: "https://pinterest.com/",
+}];
