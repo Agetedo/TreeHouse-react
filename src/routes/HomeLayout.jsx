5bd2055784b085ac96216e031d8f6a7d37104485
Agetedo <agetedo@gmail.com>
2024-10-20 02:24:57 +0300

    update <HomeLayout />

--- src/routes/HomeLayout.jsx
+++ src/routes/HomeLayout.jsx
@@ -6,24 +6,24 @@ import HeaderBanner from "../components/HeaderBanner.jsx";
 import Heading from "../components/Heading.jsx";
 
 export default function HomeLayout() {
-    const scrollToFooter = useRef(null);
-    const handleScrollDown = () => {
-        scrollToFooter.current.scrollIntoView({ 
-            behavior: "smooth", 
-            block: "nearest", 
-        });
-    };
+  const scrollToFooter = useRef(null);
+  const handleScrollDown = () => {
+    scrollToFooter.current.scrollIntoView({ 
+      behavior: "smooth", 
+      block: "nearest", 
+    });
+  };
     
-    return(
-        <>
-            <main>
-              <HeaderBanner />
-              <Heading onClick={handleScrollDown}/>
-                <Suspense fallback={<Loader />}>
-                  <Outlet />
-                </Suspense>
-            </main>
-            <Footer ref={scrollToFooter} />
-        </>
-    );
+  return(
+    <>
+      <main>
+        <HeaderBanner />
+        <Heading onClick={handleScrollDown}/>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer ref={scrollToFooter} />
+    </>
+  );
 }
