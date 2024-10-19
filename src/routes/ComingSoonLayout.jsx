930dacd553ef00762bea1f9f94302cda0060c90c
Agetedo <agetedo@gmail.com>
2024-10-20 02:25:49 +0300

    update <ComingSoonLayout />

--- src/routes/ComingSoonLayout.jsx
+++ src/routes/ComingSoonLayout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 import Loader from "../components/Loader.jsx";
 
 export default function ComingSoonLayout() {
-    return(
-        <main>
-            <Suspense fallback={<Loader />}>
-              <Outlet />
-            </Suspense>
-        </main>
-    );
+  return(
+    <main>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </main>
+  );
 }
