350a8ba95b0bb07864f62dba760338da88d8d3b4
Agetedo <agetedo@gmail.com>
2024-10-20 02:46:03 +0300

    update <GiftCard />

--- src/pages/GiftCard.jsx
+++ src/pages/GiftCard.jsx
@@ -2,10 +2,10 @@ import SendGiftCard from "../components/SendGiftCard";
 import Recommendations from "../components/Recommendations";
 
 export default function GiftCard() {
-    return (
-        <>
-          <SendGiftCard />
-          <Recommendations />
-        </>
-    );
+  return (
+    <>
+      <SendGiftCard />
+      <Recommendations />
+    </>
+  );
 }
