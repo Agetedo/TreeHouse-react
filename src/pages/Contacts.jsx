f74bde0f2ff673ec62d458b4fe5aa7fda5bc4fda
Agetedo <agetedo@gmail.com>
2024-10-20 02:47:31 +0300

    update <Contacts />

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,10 +2,10 @@ import SocialContacts from "../components/SocialContacts";
 import ContactsForm from "../components/ContactsForm";
 
 export default function Contacts() {
-    return (
-        <>
-          <SocialContacts />
-          <ContactsForm />
-        </>
-    );
+  return (
+    <>
+      <SocialContacts />
+      <ContactsForm />
+    </>
+  );
 }
