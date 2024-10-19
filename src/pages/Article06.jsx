bc18e4d399bcb1c2d7390df8f6ff5ff9acd9390f
Agetedo <agetedo@gmail.com>
2024-10-20 02:56:51 +0300

    update <Article06 />

--- src/pages/Article06.jsx
+++ src/pages/Article06.jsx
@@ -14,65 +14,66 @@ const title02 = <>{"Tips for a More Sustainable Laundry Routine"}</>;
 const text03 = <>{"Fortunately, there are ways to make our laundry routines more sustainable and extend the life of our kids' clothes."}</>;
 const text04 = <>{"By following these simple tips, you can create a laundry routine that not only saves you time and money but also helps the environment by reducing water usage and waste. Plus, your kids' clothes will last longer, saving you the hassle of constantly replacing them. Remember, small changes can have a big impact, and with a little effort, you can create a more sustainable laundry routine that benefits both your family and the planet. So, start implementing these hacks today and see the difference it makes!"}</>;
 
+export default function Article06() {
+  const pointsList = article06Points.map(point =>
+    <div key={point.id} className="article-point__items">
+      <img src={point.imgSrc} alt="#" />
+      <p className="article-point__text">{point.text}</p>
+    </div>
+  );
+  const greenPointsList = article06GreenPoints.map(point =>
+    <div key={point.id} className="article-point__items">
+      <img src={point.imgSrc} alt="#" />
+      <p className="article-point__text">{point.text}</p>
+    </div>
+  );
+
+  return (
+    <>
+      <ArticlePageHeading 
+        title={"Laundry Hacks for Eco-Conscious Parents: How to Extend the Life of Your Kids' Clothing and Save Money"}
+        imgSrc={article06Image}
+        imgAlt={"Basket with laundry"}
+      />
+      <ArticleText 
+        text01={text01}
+      />
+      <PointsTitle 
+        title={title01}
+        text={text02}
+      />
+      <ArticlePoints list={pointsList} />
+      <PointsTitle 
+        title={title02}
+        text={text03}
+      />
+      <ArticlePoints list={greenPointsList} />
+      <ArticleText 
+        text01={text04}
+      />
+      <LatestArticles />
+      <Tend />
+    </>
+  );
+}
+
 function ArticleText({ text01 }) {
-    return(
-       <div className="article-text">
-            <div className="conteiner">
-                <p className="post" id="article06Text01">{text01}</p>
-            </div>
-       </div> 
-    );
+  return(
+    <div className="article-text">
+      <div className="conteiner">
+        <p className="post" id="article06Text01">{text01}</p>
+      </div>
+    </div> 
+  );
 }
 
 function PointsTitle({ title, text }) {
-    return(
-        <div className="points-title">
-            <div className="conteiner">
-                <h3 className="points__title">{title}</h3>
-                <p className="points__post">{text}</p>
-            </div>
-        </div>
-    );
-}
-
-export default function Article06() {
-    const pointsList = article06Points.map(point =>
-        <div key={point.id} className="article-point__items">
-            <img src={point.imgSrc} alt="#" />
-            <p className="article-point__text">{point.text}</p>
-        </div>
-    );
-    const greenPointsList = article06GreenPoints.map(point =>
-        <div key={point.id} className="article-point__items">
-            <img src={point.imgSrc} alt="#" />
-            <p className="article-point__text">{point.text}</p>
-        </div>
-    );
-    return (
-        <>
-            <ArticlePageHeading 
-              title={"Laundry Hacks for Eco-Conscious Parents: How to Extend the Life of Your Kids' Clothing and Save Money"}
-              imgSrc={article06Image}
-              imgAlt={"Basket with laundry"}
-            />
-            <ArticleText 
-              text01={text01}
-            />
-            <PointsTitle 
-              title={title01}
-              text={text02}
-            />
-            <ArticlePoints list={pointsList} />
-            <PointsTitle 
-              title={title02}
-              text={text03}
-            />
-            <ArticlePoints list={greenPointsList} />
-            <ArticleText 
-              text01={text04}
-            />
-            <LatestArticles />
-            <Tend />
-        </>
-    );
+  return(
+    <div className="points-title">
+      <div className="conteiner">
+        <h3 className="points__title">{title}</h3>
+        <p className="points__post">{text}</p>
+      </div>
+    </div>
+  );
 }
