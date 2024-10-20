6c74be67434de47213eb8fa531b1c8003e5dc3f5
Agetedo <agetedo@gmail.com>
2024-10-21 02:11:21 +0300

    update <CardProductShow />

--- src/components/CardProductShow.jsx
+++ src/components/CardProductShow.jsx
@@ -10,32 +10,32 @@ import 'swiper/scss/pagination';
 import "..//styles/CardProductShow.scss";
 
 const showImgList = productShow.map(show => 
-    <SwiperSlide key={show.id} className="show-slide">
-        <img src={show.imageSrc} alt={show.imageAlt} /> 
-    </SwiperSlide>
+  <SwiperSlide key={show.id} className="show-slide">
+    <img src={show.imageSrc} alt={show.imageAlt} /> 
+  </SwiperSlide>
 );
 
 export default function CardProductShow() {
-    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
-    return (
-        <div className="product__appearance">
-            <Swiper modules={[FreeMode, Navigation, Thumbs, Pagination]} 
-                loop={true} spaceBetween={10} pagination={{ type: "fraction" }}
-                navigation={true}
-                thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-                id="productShow">
-                {showImgList}
-            </Swiper>
+  return (
+    <div className="product__appearance">
+      <Swiper modules={[FreeMode, Navigation, Thumbs, Pagination]} 
+        loop={true} spaceBetween={10} pagination={{ type: "fraction" }}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+        id="productShow">
+        {showImgList}
+      </Swiper>
 
-            <Swiper modules={[FreeMode, Navigation, Thumbs, Pagination]}
-                onSwiper={setThumbsSwiper} loop={true}
-                spaceBetween={8} slidesPerView={6}
-                freeMode={true}
-                watchSlidesProgress={true}
-                id="imgSelect">
-                {showImgList}    
-            </Swiper>
-        </div>
-    );
+      <Swiper modules={[FreeMode, Navigation, Thumbs, Pagination]}
+        onSwiper={setThumbsSwiper} loop={true}
+        spaceBetween={8} slidesPerView={6}
+        freeMode={true}
+        watchSlidesProgress={true}
+        id="imgSelect">
+        {showImgList}    
+      </Swiper>
+    </div>
+  );
 }
