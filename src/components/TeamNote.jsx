9e417635cfe3b9bde71de00e778fba9c7cc1020e
Agetedo <agetedo@gmail.com>
2024-10-20 03:38:52 +0300

    update <TeamNote />

--- src/components/TeamNote.jsx
+++ src/components/TeamNote.jsx
@@ -3,22 +3,22 @@ import teamLabelImage from "/team-label-image.png";
 import teamNoteImage from "/team-note-image.png";
 import "..//styles/TeamNote.scss";
 
-function NoteItems() {
-    return (
-        <div className="team-note__items">
-            <img id="teamLeafLabel" src={teamLeafLabel} alt="#" />
-            <img id="teamLabelImage" src={teamLabelImage} alt="#" />
-        </div>
-    );
+export default function TeamNote() {
+  return (
+    <div className="team-note">
+      <div className="conteiner">
+        <NoteItems />
+        <img id="teamNoteImage" src={teamNoteImage} alt="#" />
+      </div>
+    </div>
+  );
 }
 
-export default function TeamNote() {
-    return (
-        <div className="team-note">
-            <div className="conteiner">
-              <NoteItems />
-              <img id="teamNoteImage" src={teamNoteImage} alt="#" />
-            </div>
-        </div>
-    );
+function NoteItems() {
+  return (
+    <div className="team-note__items">
+      <img id="teamLeafLabel" src={teamLeafLabel} alt="#" />
+      <img id="teamLabelImage" src={teamLabelImage} alt="#" />
+    </div>
+  );
 }
