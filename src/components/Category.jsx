7188d8dbc86f2ef05e3943053e848e0c573eda99
Agetedo <agetedo@gmail.com>
2024-10-21 02:18:37 +0300

    update <Category />

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -11,52 +11,53 @@ import "swiper/scss/autoplay";
 import "swiper/scss/keyboard";
 import "..//styles/Category.scss";
 
-function CategoryTitle({ caption, title }) {
-    return(
-        <>
-          <p className="caption__center">{caption}</p>
-          <h3 className="title__center">{title}</h3>
-        </>
-    );
-}
-
 const slidesList = categoryGallery.map(slide => 
-    <SwiperSlide key={slide.id} className="swiper-slide">
-        <Link to={slide.linkTo} className="category__link">
-          <img src={slide.imageSrc} alt={slide.imageAlt}/>
-        </Link>
-    </SwiperSlide>
+  <SwiperSlide key={slide.id} className="swiper-slide">
+    <Link to={slide.linkTo} className="category__link">
+      <img src={slide.imageSrc} alt={slide.imageAlt}/>
+    </Link>
+  </SwiperSlide>
 );
 
 export default function Category() {
-    return (
-        <div className="category">
-            <div className="conteiner">
+  return (
+    <div className="category">
+      <div className="conteiner">
 
-                <img src={leafLabel} alt="#" id="leafLabel" />
-                <CategoryTitle caption={"let's make shopping"} title={"What are you looking for?"} />
+        <img src={leafLabel} alt="#" id="leafLabel" />
+        <CategoryTitle caption={"let's make shopping"} title={"What are you looking for?"} />
 
-                <Swiper modules={[Navigation, EffectFade, Pagination, Autoplay, Keyboard,]} 
-                    id="categoryGallery"
-                    direction={"horizontal"}
-                    slidesPerView={1} autoHeight={true}
-                    loop={true} speed={1000}
-                    autoplay={{ delay: 4000, disableOnInteraction: false }}
-                    effect={"fade"} 
-                    pagination={{ clickable: true,
-                        el: ".category__pagination",
-                        renderBullet: function(index, categorys) {
-                          let labels = ['TOPS', 'BOTTOMS', 'ROOMPERS', 'HATS', 'ALL COLLECTION'];
-                          return ('<span class="'+categorys+'">' + (labels[index]) + "</span>");
-                        },
-                    }}
-                    keyboard={{ enabled: true, onlyInViewport: false }}>
-
-                    {slidesList}
-                    <div className="category__pagination"></div>
-                </Swiper>
+        <Swiper modules={[Navigation, EffectFade, Pagination, Autoplay, Keyboard,]} 
+          id="categoryGallery"
+          direction={"horizontal"}
+          slidesPerView={1} autoHeight={true}
+          loop={true} speed={1000}
+          autoplay={{ delay: 4000, disableOnInteraction: false }}
+          effect={"fade"} 
+          pagination={{ 
+            clickable: true,
+            el: ".category__pagination",
+            renderBullet: function(index, categorys) {
+              let labels = ['TOPS', 'BOTTOMS', 'ROOMPERS', 'HATS', 'ALL COLLECTION'];
+              return ('<span class="'+categorys+'">' + (labels[index]) + "</span>");
+            },
+          }}
+          keyboard={{ enabled: true, onlyInViewport: false }}
+        >
+          {slidesList}
+          <div className="category__pagination"></div>
+        </Swiper>
                 
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
+}
+
+function CategoryTitle({ caption, title }) {
+  return(
+    <>
+      <p className="caption__center">{caption}</p>
+      <h3 className="title__center">{title}</h3>
+    </>
+  );
 }
