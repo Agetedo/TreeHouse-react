d8b54bd2f6ffaa790a7a220e5eec11bbe6f00f4c
Agetedo <agetedo@gmail.com>
2024-10-21 01:49:29 +0300

    update <ArticlePageHeading />

--- src/components/ArticlePageHeading.jsx
+++ src/components/ArticlePageHeading.jsx
@@ -4,40 +4,40 @@ import articlePageLabel from "/article-page-label.png";
 import pageTitleArrow from "/svg/page-title-arrow.svg";
 import "..//styles/ArticlePageHeading.scss";
 
+export default function ArticlePageHeading({ title, imgSrc, imgAlt }) {
+  return(
+    <div className="article-heading">
+      <div className="conteiner">
+        <JournalPageLink journalLink={"Back to Journal"}/>
+        <HeadingTitle title={title} />
+        <HeadingImage imgSrc={imgSrc} imgAlt={imgAlt} />
+      </div>
+    </div>
+  );
+}
+
 function JournalPageLink({ journalLink }) {
-    return(
-        <div className="page-link__items">
-          <img id="ArticleArrow" src={pageTitleArrow} alt="#" />
-          <Link to={PathConstants.JOURNALPAGE}>{journalLink}</Link>
-        </div>
-    );
+  return(
+    <div className="page-link__items">
+      <img id="ArticleArrow" src={pageTitleArrow} alt="#" />
+      <Link to={PathConstants.JOURNALPAGE}>{journalLink}</Link>
+    </div>
+  );
 }
 
 function HeadingTitle ({ title }) {
-    return(
-        <div className="article-heading__title">
-          <h2>{title}</h2>
-          <img src={articlePageLabel} alt="#" />
-        </div>
-    );
+  return(
+    <div className="article-heading__title">
+      <h2>{title}</h2>
+      <img src={articlePageLabel} alt="#" />
+    </div>
+  );
 }
 
 function HeadingImage ({ imgSrc, imgAlt }) {
-    return(
-        <div className="article-heading__image">
-          <img src={imgSrc} alt={imgAlt} />
-        </div>
-    );
-}
-
-export default function ArticlePageHeading({ title, imgSrc, imgAlt }) {
-    return(
-        <div className="article-heading">
-            <div className="conteiner">
-                <JournalPageLink journalLink={"Back to Journal"}/>
-                <HeadingTitle title={title} />
-                <HeadingImage imgSrc={imgSrc} imgAlt={imgAlt} />
-            </div>
-        </div>
-    );
+  return(
+    <div className="article-heading__image">
+      <img src={imgSrc} alt={imgAlt} />
+    </div>
+  );
 }
