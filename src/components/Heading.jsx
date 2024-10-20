09b0eccce02da4e34d72f4f8471ab8090a2785a6
Agetedo <agetedo@gmail.com>
2024-10-20 23:46:32 +0300

    update <Heading />

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,43 +5,43 @@ import PathConstants from "../routes/pathConstants";
 import scrollIcon from "/svg/scroll-down-icon.svg";
 import "..//styles/Heading.scss";
 
+export default function Heading({ onClick }) {
+  return(
+    <div className="heading">
+      <Header indexHeader={{ backgroundColor: "transparent", }} />
+            
+      <div className="heading__conteiner">
+        <section className="heading__content">
+          <span id="headingCaption">Treehouse:</span>
+          <h2>Little Clothes.</h2>
+          <h2 id="titleDesign">Big Impact.</h2>
+          <p>
+            High-quality children&apos;s clothing, crafted with care from sustainable materials that are kind to the planet. These timeless styles are the
+            perfect companions for your little adventurer, wherever their imagination takes them.
+          </p>
+                    
+          <HeadingButtons textLink={"SHOP NOW"} />
+        </section>
+      </div>
+      <ScrollDown scrollText={"Scroll"} onClick={onClick}/>
+    </div>
+  );
+}
+
 function HeadingButtons({ textLink }) {
-    return(
-        <div className="heading__buttons">
-          <Link to={PathConstants.SHOPPAGE} className="button__link">{textLink}</Link>
-          <Link to={PathConstants.SHOPPAGE} className="arrow__link"><img src={arrowLink} alt="#" /></Link>
-        </div>
-    );
+  return(
+    <div className="heading__buttons">
+      <Link to={PathConstants.SHOPPAGE} className="button__link">{textLink}</Link>
+      <Link to={PathConstants.SHOPPAGE} className="arrow__link"><img src={arrowLink} alt="#" /></Link>
+    </div>
+  );
 }
 
 function ScrollDown({ scrollText, onClick}) {
-    return(
-        <div id="scrollDown" onClick={onClick}>
-            <span>{scrollText}</span>
-            <img src={scrollIcon} alt="#" />
-        </div>
-    );
-}
-
-export default function Heading({ onClick }) {
-    return(
-        <div className="heading">
-            <Header indexHeader={{ backgroundColor: "transparent", }} />
-            
-            <div className="heading__conteiner">
-                <section className="heading__content">
-                    <span id="headingCaption">Treehouse:</span>
-                    <h2>Little Clothes.</h2>
-                    <h2 id="titleDesign">Big Impact.</h2>
-                    <p>
-                        High-quality children&apos;s clothing, crafted with care from sustainable materials that are kind to the planet. These timeless styles are the
-                        perfect companions for your little adventurer, wherever their imagination takes them.
-                    </p>
-                    
-                    <HeadingButtons textLink={"SHOP NOW"} />
-                </section>
-            </div>
-            <ScrollDown scrollText={"Scroll"} onClick={onClick}/>
-        </div>
-    );
+  return(
+    <div id="scrollDown" onClick={onClick}>
+      <span>{scrollText}</span>
+      <img src={scrollIcon} alt="#" />
+    </div>
+  );
 }
