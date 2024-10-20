25c9502475ecc5a47f59b0fda04c6d294e398514
Agetedo <agetedo@gmail.com>
2024-10-20 22:28:53 +0300

    update <ShopAllBaner />

--- src/components/ShopAllBaner.jsx
+++ src/components/ShopAllBaner.jsx
@@ -4,36 +4,37 @@ import pageTitleArrow from "/svg/page-title-arrow.svg";
 import "..//styles/PageTitle.scss";
 import "..//styles/ShopAllBaner.scss";
 
-function PageTitle({ pageTitleLink, homeLink }) {
-    return (
-      <div className="page-link">
+export default function ShopAllBaner() {
+  return (
+    <>
+      <PageTitle homeLink={"Home"} pageTitleLink={"Shop all"} />
+      <div className="shop__baner">
         <div className="conteiner">
-          <div className="page-link__items">
-            <Link to={PathConstants.HOME}>{homeLink}</Link>
-            <img src={pageTitleArrow} alt="#" />
-            <span>{pageTitleLink}</span>
-          </div>
+          <Baner caption={"shop all"} title={"Kid and nature approved styles"}/>
         </div>
       </div>
-    );
-}
-function Baner({caption, title }) {
-    return(
-        <div className="baner">
-          <p>{caption}</p><h2>{title}</h2>
-        </div>
-    );
+    </>
+  );
 }
 
-export default function ShopAllBaner() {
-    return (
-        <>
-            <PageTitle homeLink={"Home"} pageTitleLink={"Shop all"} />
-            <div className="shop__baner">
-                <div className="conteiner">
-                  <Baner caption={"shop all"} title={"Kid and nature approved styles"}/>
-                </div>
-            </div>
-        </>
-    );
+function PageTitle({ pageTitleLink, homeLink }) {
+  return (
+    <div className="page-link">
+      <div className="conteiner">
+        <div className="page-link__items">
+          <Link to={PathConstants.HOME}>{homeLink}</Link>
+          <img src={pageTitleArrow} alt="#" />
+          <span>{pageTitleLink}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Baner({caption, title }) {
+  return(
+    <div className="baner">
+      <p>{caption}</p><h2>{title}</h2>
+    </div>
+  );
 }
