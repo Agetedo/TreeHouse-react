39199898fa6d639926557acfe5e83e176b7029da
Agetedo <agetedo@gmail.com>
2024-10-21 01:45:07 +0300

    update <AmountTax />

--- src/components/AmountTax.jsx
+++ src/components/AmountTax.jsx
@@ -1,10 +1,10 @@
 import "..//styles/AmountTax.scss";
 
 export default function AmountTax({ numberUsd, taxIcluded }) {
-    return (
-        <div className="amount__tax">
-          <p id="tax">{numberUsd}</p>
-          <span>{taxIcluded}</span>
-        </div>
-    );
+  return (
+    <div className="amount__tax">
+      <p id="tax">{numberUsd}</p>
+      <span>{taxIcluded}</span>
+    </div>
+  );
 }
