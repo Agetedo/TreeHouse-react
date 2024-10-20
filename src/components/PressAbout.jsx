20ca93808a24a7673895d8e4da51ac209dba3b7a
Agetedo <agetedo@gmail.com>
2024-10-20 23:27:18 +0300

    update <PressAbout />

--- src/components/PressAbout.jsx
+++ src/components/PressAbout.jsx
@@ -9,55 +9,56 @@ import "swiper/scss/keyboard";
 import 'swiper/scss/effect-cube';
 import "..//styles/PressAbout.scss";
 
-function PressAboutTitle({ title }) {
-    return (
-        <>
-          <img id="pressAboutLabel" src={pressAboutLabel} alt="#" />
-          <img id="pressAboutLabel02" src={pressAboutLabel02} alt="#"/>
-          <h2 className="press-about__title">{title}</h2>
-        </>  
-    );
+export default function PressAbout() {
+  return (
+    <div className="press-about">
+      <div className="conteiner">
+        <PressAboutTitle title={"Press about us"} />
+
+        <Swiper modules={[Pagination, Keyboard, EffectCube ]}
+          id="pressAbout" loop={true} speed={1000} grabCursor={true}
+          keyboard={{ enabled: true, onlyInViewport: false }}
+          pagination={{ 
+            el: "#pressAboutPagination", 
+            type: "progressbar",
+          }} 
+          autoHeight={true}
+          effect={"cube"}
+          cubeEffect={{
+            shadow: false,
+            slideShadows: false,
+            shadowOffset: 0,
+            shadowScale: 0,
+          }}
+        >           
+          {pressList}
+          <div className="swiper-pagination-progressbar" id="pressAboutPagination"/>
+        </Swiper> 
+                
+      </div>
+    </div>     
+  );
 }
+
 const pressList = pressAboutSlides.map(press => 
-    <SwiperSlide key={press.id} className="swiper-slide">
-        <blockquote cite={press.cite} className="press-about__text">
-          {press.blockquoteText}
-          <img src={press.autor} alt={press.autorAlt} className="blockquote__autor"/>
-        </blockquote>
-        <div className="read-link">
-            <a rel="noopener" target="_blank" href={press.readLink} className="press-about__link">{press.linkText}</a>
-            <img src={press.imgSrc} alt="#" />
-        </div>
-    </SwiperSlide>
+  <SwiperSlide key={press.id} className="swiper-slide">
+    <blockquote cite={press.cite} className="press-about__text">
+      {press.blockquoteText}
+      <img src={press.autor} alt={press.autorAlt} className="blockquote__autor"/>
+    </blockquote>
+    <div className="read-link">
+      <a rel="noopener" target="_blank" href={press.readLink} className="press-about__link">{press.linkText}</a>
+      <img src={press.imgSrc} alt="#" />
+    </div>
+  </SwiperSlide>
 );
 
-export default function PressAbout() {
-    return (
-        <div className="press-about">
-            <div className="conteiner">
-                <PressAboutTitle title={"Press about us"} />
-
-                <Swiper modules={[Pagination, Keyboard, EffectCube ]}
-                    id="pressAbout" loop={true} speed={1000} grabCursor={true}
-                    keyboard={{ enabled: true, onlyInViewport: false }}
-                    pagination={{ 
-                        el: "#pressAboutPagination", 
-                        type: "progressbar",
-                    }} 
-                    autoHeight={true}
-                    effect={"cube"}
-                    cubeEffect={{
-                        shadow: false,
-                        slideShadows: false,
-                        shadowOffset: 0,
-                        shadowScale: 0,
-                    }}>
-                        
-                  {pressList}
-                  <div className="swiper-pagination-progressbar" id="pressAboutPagination"/>
-                </Swiper> 
-                
-            </div>
-        </div>     
-    );
+function PressAboutTitle({ title }) {
+  return (
+    <>
+      <img id="pressAboutLabel" src={pressAboutLabel} alt="#" />
+      <img id="pressAboutLabel02" src={pressAboutLabel02} alt="#"/>
+      <h2 className="press-about__title">{title}</h2>
+    </>  
+  );
 }
