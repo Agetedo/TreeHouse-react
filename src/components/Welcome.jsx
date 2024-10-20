0e3e6bba1ba1b4f8cd15603417d8ceeb916dfd62
Agetedo <agetedo@gmail.com>
2024-10-20 03:15:46 +0300

    update <Welcome />

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,36 +4,36 @@ import TitleLeft from "./TitleLeft";
 import PathConstants from "../routes/pathConstants";
 import "..//styles/Welcome.scss";
 
-function WelcomeImage({ imageAlt, wordDesign }) {
-    return(
-        <div className="welcome__image">
-          <img src={welcomeImage} alt={imageAlt}/>
-          <span id="wordDesign">{wordDesign}</span>
+export default function Welcome() {
+  return(
+    <div className="welcome">
+      <div className="conteiner">
+        <div className="welcome__items">
+          <WelcomeImage imageAlt={"Сollage of three photos"} wordDesign={"TreeHouse"} />
+          <WelcomeContent 
+            contentText={"Mindfully crafted children's clothing focused on your little one and designed with great care for our planet and the people on it."}
+            linkText={"READ MORE"}
+          />
         </div>
-    );
-}
-function WelcomeContent({ contentText, linkText }) {
-    return(
-        <section className="welcome__content">
-          <TitleLeft caption={"welcome"} titleFirst={"Hi, meet"} titleSecond={"TreeHouse."}/>  
-          <p id="contentText">{contentText}</p>  
-          <Link to={PathConstants.OURTEAMPAGE} className="read-team__link">{linkText}</Link> 
-        </section>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default function Welcome() {
-    return(
-        <div className="welcome">
-            <div className="conteiner">
-                <div className="welcome__items">
-                    <WelcomeImage imageAlt={"Сollage of three photos"} wordDesign={"TreeHouse"} />
-                    <WelcomeContent 
-                      contentText={"Mindfully crafted children's clothing focused on your little one and designed with great care for our planet and the people on it."}
-                      linkText={"READ MORE"}
-                    />
-                </div>
-            </div>
-        </div>
-    );
+function WelcomeImage({ imageAlt, wordDesign }) {
+  return(
+    <div className="welcome__image">
+      <img src={welcomeImage} alt={imageAlt}/>
+      <span id="wordDesign">{wordDesign}</span>
+    </div>
+  );
+}
+function WelcomeContent({ contentText, linkText }) {
+  return(
+    <section className="welcome__content">
+      <TitleLeft caption={"welcome"} titleFirst={"Hi, meet"} titleSecond={"TreeHouse."}/>  
+      <p id="contentText">{contentText}</p>  
+      <Link to={PathConstants.OURTEAMPAGE} className="read-team__link">{linkText}</Link> 
+    </section>
+  );
 }
