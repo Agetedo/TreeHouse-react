82c256130a34f36439696d3cba1329e5aa477fe4
Agetedo <agetedo@gmail.com>
2024-10-24 00:25:54 +0300

    update <Article05 />

--- src/pages/Article05.jsx
+++ src/pages/Article05.jsx
@@ -7,17 +7,17 @@ import { article05Points } from "../DataBase/article05Points";
 import Tend from "../components/Tend";
 import "..//styles/Article.scss";
 
-const text01 = <>{"As parents, we all know the struggle of getting our kids dressed every day. From mismatched outfits to piles of clothes on the floor, it can be overwhelming and time-consuming. But what if there was a solution that not only simplified the process but also helped contribute to a more sustainable lifestyle? Enter the capsule wardrobe."}</>;
-const title01 = <>{"What is a capsule wardrobe?"}</>;
-const text02 = <>{"A capsule wardrobe is a curated collection of clothing that consists of a few essential pieces that can be mixed and matched to create a variety of outfits (just like Treehouse's Spring Summer Collection!). The concept was popularized by fashion designer Donna Karan in the 1980s and has since gained popularity as a way to simplify dressing and reduce waste."}</>;
-const text03 = <>{"The idea is to have a smaller number of high-quality pieces that can be worn in many different ways, rather than a large number of low-quality items that only have one or two uses. This approach can simplify the process of getting dressed, reduce the clutter in your child's closet, and contribute to more sustainable living by promoting conscious consumption."}</>;
-const title02 = <>{"Why does a capsule wardrobe make dressing easier?"}</>;
-const text04 = <>{"By limiting the number of clothes in your child's wardrobe to a few essential pieces, you eliminate decision fatigue and simplify the process of getting dressed. No more rifling through piles of clothes or trying to find a matching outfit. With a capsule wardrobe, every piece is carefully chosen to work together, making dressing quick and easy."}</>;
-const title03 = <>{"How does a capsule wardrobe contribute to sustainable living?"}</>;
-const text05 = <>{"Fast fashion has contributed to a culture of overconsumption, where clothing is purchased and discarded at an alarming rate. A capsule wardrobe helps combat this by encouraging intentional purchases and reducing waste. By investing in high-quality, versatile pieces that can be worn for multiple seasons, you not only reduce the amount of clothing going into landfills but also save money in the long run."}</>;
-const title04 = <>{"Top tips for creating a capsule wardrobe for your child"}</>;
-const text06 = <>{"A capsule wardrobe saves not only energy and time, but also money. A thoughtful capsule is an antidote to unsuccessful purchases, returns and pointless spending."}</>;
-const text07 = <>{"At the very least you'll have less stress associated with getting your kids dressed in the morning. You will have to do less laundry because the children will have less clothes and they will not have the physical ability to change sets 5 times a day. This teaches frugality, by the way too!"}</>;
+const text01 = "As parents, we all know the struggle of getting our kids dressed every day. From mismatched outfits to piles of clothes on the floor, it can be overwhelming and time-consuming. But what if there was a solution that not only simplified the process but also helped contribute to a more sustainable lifestyle? Enter the capsule wardrobe.";
+const title01 = "What is a capsule wardrobe?";
+const text02 = "A capsule wardrobe is a curated collection of clothing that consists of a few essential pieces that can be mixed and matched to create a variety of outfits (just like Treehouse's Spring Summer Collection!). The concept was popularized by fashion designer Donna Karan in the 1980s and has since gained popularity as a way to simplify dressing and reduce waste.";
+const text03 = "The idea is to have a smaller number of high-quality pieces that can be worn in many different ways, rather than a large number of low-quality items that only have one or two uses. This approach can simplify the process of getting dressed, reduce the clutter in your child's closet, and contribute to more sustainable living by promoting conscious consumption.";
+const title02 = "Why does a capsule wardrobe make dressing easier?";
+const text04 = "By limiting the number of clothes in your child's wardrobe to a few essential pieces, you eliminate decision fatigue and simplify the process of getting dressed. No more rifling through piles of clothes or trying to find a matching outfit. With a capsule wardrobe, every piece is carefully chosen to work together, making dressing quick and easy.";
+const title03 = "How does a capsule wardrobe contribute to sustainable living?";
+const text05 = "Fast fashion has contributed to a culture of overconsumption, where clothing is purchased and discarded at an alarming rate. A capsule wardrobe helps combat this by encouraging intentional purchases and reducing waste. By investing in high-quality, versatile pieces that can be worn for multiple seasons, you not only reduce the amount of clothing going into landfills but also save money in the long run.";
+const title04 = "Top tips for creating a capsule wardrobe for your child";
+const text06 = "A capsule wardrobe saves not only energy and time, but also money. A thoughtful capsule is an antidote to unsuccessful purchases, returns and pointless spending.";
+const text07 = "At the very least you'll have less stress associated with getting your kids dressed in the morning. You will have to do less laundry because the children will have less clothes and they will not have the physical ability to change sets 5 times a day. This teaches frugality, by the way too!";
 
 export default function Article05() {
   const pointsList = article05Points.map(point =>
